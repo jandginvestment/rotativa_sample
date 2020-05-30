94dc84ed9b598f5c1cb2c7cfdf86417c39930a7e
juliusmasilamani <juliusmas@gmail.com>
2020-05-30 10:48:23 +0400

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using rotativa_sample.Models;
+
+namespace rotativa_sample.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new Rotativa.AspNetCore.ViewAsPdf("Index", GetStudents());
+            //return View();
+        }
+       
+        public List<Student> GetStudents ()
+        {
+            var students= new List<Student> { 
+            new Student{ID=1, Class="12",Name="ஸ்ரீதர் ",Section="அ"}      ,
+             new Student{ID=5, Class="12",Name="இராமசாமி ",Section="அ"}      ,
+             new Student{ID=8, Class="12",Name="இந்திரன் ",Section="ஆ"}      ,
+             new Student{ID=10, Class="12",Name="குப்புசாமி",Section="ஆ"}      ,
+             new Student{ID=12, Class="12",Name="இரமேஷ்",Section="அ"}      ,
+             new Student{ID=16, Class="12",Name="வேலன்",Section="அ"}      ,
+
+            };
+            return students.OrderByDescending(x => x.Section).ThenBy(x => x.Name).ToList();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rotativa_sample.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Section { get; set; }
+    }
+}
